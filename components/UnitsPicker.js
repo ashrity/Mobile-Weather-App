e81c6c6a7f2038e807513d6c01f80db900ce188c
Ashrit Yerukola <go9874@wayne.edu>
2021-10-05 18:01:55 -0400

    App

---
+++ components/UnitsPicker.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text, Picker, StyleSheet } from 'react-native'
+//import{ Picker } from '@react-native-picker/picker'
+
+export default function UnitsPicker({unitsSystem, setUnitsSystem}) {
+    return (
+        <View style={styles.unitsSystem}>
+            <Picker selectedValue={unitsSystem} onValueChange={(item)=>setUnitsSystem(item)}>
+                <Picker.Item label="C°" value="metric"/>
+                <Picker.Item label="F°" value="imperial"/>
+            </Picker>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    unitsSystem:{
+        position: 'absolute', 
+        top: 20,
+        left: 20,
+        height: 50,
+        width: 100,
+    }
+})
